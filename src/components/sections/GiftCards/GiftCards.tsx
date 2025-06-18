bf8c0d5c2d32fd0795025c9a777caec3456e64d0
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2025-06-19 00:10:59 +0200

    griftcart

--- src/components/sections/GiftCards/GiftCards.tsx
+++ src/components/sections/GiftCards/GiftCards.tsx
@@ -1,5 +1,31 @@
-const GiftCards = ({}) => {
-  return <div>GiftCards</div>;
-};
+"use client";
 
-export default GiftCards;
+import styles from "./GiftCards.module.css";
+import Image from "next/image";
+
+const giftcards = [
+  { id: 1, image: "/images/vinyl+bag.jpg", label: "Подарунковий сертифікат 200₴" },
+  { id: 2, image: "/images/vinyl+bag.jpg", label: "Подарунковий сертифікат 300₴" },
+  { id: 3, image: "/images/vinyl+bag.jpg", label: "Подарунковий сертифікат 500₴" },
+  { id: 4, image: "/images/vinyl+bag.jpg", label: "Подарунковий сертифікат 800₴" },
+  { id: 5, image: "/images/vinyl+bag.jpg", label: "Подарунковий сертифікат 1000₴" },
+];
+
+export default function GiftCards() {
+  return (
+    <section className={styles.giftCardsSection}>
+      <h2 className={styles.title}>Подарункові сертифікати</h2>
+      <div className={styles.grid}>
+        {giftcards.map(({ id, image, label }) => (
+          <div key={id} className={styles.card}>
+            <img src={image} alt={label} className={styles.image} />
+            <div className={styles.heartIcon}>
+            <Image src="/icons/heart.svg" alt="heart" width={47} height={47} />
+            </div>
+            <div className={styles.label}>{label}</div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
