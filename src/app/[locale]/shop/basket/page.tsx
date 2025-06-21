d12ff847bc1da29a429e72e2db143b4af7ceaf7a
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2025-06-21 18:05:46 +0200

    fixed5

--- src/app/[locale]/shop/basket/page.tsx
+++ src/app/[locale]/shop/basket/page.tsx
@@ -1,18 +1,10 @@
-import React from "react";
-
-interface basketProps {
-    children?: React.ReactNode;
-}
-
-const Basket = ({children} : basketProps ) => {
-
-    return (
-        <div>
-            <h1>Basket Page</h1>
-            {children}
-        </div>
-    );
+const BasketPage = () => {
+  return (
+    <div>
+      <h1>Basket Page</h1>
+      {/* Тут контент кошика */}
+    </div>
+  );
 };
 
-export default Basket;
-
+export default BasketPage;
